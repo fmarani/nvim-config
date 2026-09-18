3e6381a58b0cb65f296f0bb6ffbdf6bcff8851d9
Federico Marani <fmarani@users.noreply.github.com>
2026-09-18 12:44:27 +0200

    Upgraded to AstroNvim v6

--- lua/plugins/fede.lua
+++ lua/plugins/fede.lua
@@ -5,23 +5,16 @@ return {
     opts = {
       mappings = {
         n = {
-          ["<A-Right>"] = { ":bnext<cr>", desc = "next buffer"},
-          ["<A-Left>"] = { ":bprev<cr>", desc = "prev buffer"},
-        }
-      }
-    }
-  },
-  {
-      "AstroNvim/astrocommunity",
-      { import = "astrocommunity.pack.python" },
-      { import = "astrocommunity.git.gitlinker-nvim" },
-      { import = "astrocommunity.colorscheme.bluloco-nvim" },
-  },
-  {
-    "AstroNvim/astroui",
-    ---@type AstroUIOpts
-    opts = {
-      colorscheme = "bluloco",
-    }
+          ["<A-Right>"] = {
+            function() require("astrocore.buffer").nav(vim.v.count1) end,
+            desc = "Next buffer",
+          },
+          ["<A-Left>"] = {
+            function() require("astrocore.buffer").nav(-vim.v.count1) end,
+            desc = "Previous buffer",
+          },
+        },
+      },
+    },
   },
 }
